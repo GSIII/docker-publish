ce33319188df6487fe51ef1520f0babb96f45862
GSIII <kalseo901@gmail.com>
2025-05-09 18:00:14 +0900

    env 위치 변경경

--- st_info/app.js
+++ st_info/app.js
@@ -1,33 +1,33 @@
-var express = require('express');
-var mysql = require('mysql');
-const env = require('dotenv').config({ path: '../../.env' });
+var express = require("express");
+var mysql = require("mysql");
+const env = require("dotenv").config({ path: "../.env" });
 var app = express();
 
 var connection = mysql.createConnection({
-    host: process.env.host,
-    port: process.env.port,
-    user: process.env.user,
-    password: process.env.password,
-    database: process.env.database,
+  host: process.env.host,
+  port: process.env.port,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
 });
 
 connection.connect(function (err) {
-    if (!err) {
-        console.error('Database is connected~!!\n');
-    } else {
-        console.log('Error connecting Database~!!\n');
-    }
+  if (!err) {
+    console.error("Database is connected~!!\n");
+  } else {
+    console.log("Error connecting Database~!!\n");
+  }
 });
 
-app.get('/', function (req, res) {
-    connection.query('SELECT * FROM st_info', function (err, rows) {
-        if (err) {
-            console.log('Error while performing Query.\n', err);
-            res.status(500).send('Database error');
-            return;
-        }
+app.get("/", function (req, res) {
+  connection.query("SELECT * FROM st_info", function (err, rows) {
+    if (err) {
+      console.log("Error while performing Query.\n", err);
+      res.status(500).send("Database error");
+      return;
+    }
 
-        let template = `
+    let template = `
         <table border="1" style="margin:auto; text-align:center;">
             <tr>
                 <th>ST_ID</th>
@@ -36,21 +36,21 @@ app.get('/', function (req, res) {
             </tr>
         `;
 
-        rows.forEach(item => {
-            template += `
+    rows.forEach((item) => {
+      template += `
             <tr>
                 <td>${item.ST_ID}</td>
                 <td>${item.NAME}</td>
                 <td>${item.DEPT}</td>
             </tr>
             `;
-        });
-
-        template += '</table>';
-        res.send(template);
     });
+
+    template += "</table>";
+    res.send(template);
+  });
 });
 
 app.listen(8080, function () {
-    console.log('8080 Port : Server Started~!!\n');
+  console.log("8080 Port : Server Started~!!\n");
 });
