f12069a6176ddabbea30d4a30133fe563247f44d
GSIII <kalseo901@gmail.com>
2025-05-12 09:38:54 +0900

    사파리 코드 보이는 에러

--- st_info/app.js
+++ st_info/app.js
@@ -4,53 +4,53 @@ const env = require("dotenv").config({ path: "./.env" });
 var app = express();
 
 var connection = mysql.createConnection({
-  port: process.env.port,
   host: process.env.host,
   user: process.env.user,
+  port: process.env.port,
   password: process.env.password,
   database: process.env.database,
 });
 
 connection.connect(function (err) {
   if (!err) {
-    console.error("Database is connected~!!\n\n");
+    console.log("Database is connected~!!\n\n");
   } else {
-    console.log("Error connecting Database~!!\n\n");
+    console.log("Error connecting Database~!!");
   }
 });
 
 app.get("/", function (req, res) {
   connection.query("select * from st_info", function (err, rows, fields) {
+    // connection.end();
     if (!err) {
       // res.send(rows);
-      res.writeHead(200, {
-        "content-type": "text/html charset=utf-8",
-      });
+      res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
       var template = `
-            <table border="1" margin:auto; style="text-align:center;">
-                      <tr>
-                        <th>ST_ID</th>
-                        <th>NAME</th>
-                        <th>DEPT</th>
-                      </tr>
-           `;
+                <table border="1" style="margin:auto;text-align:center;">
+                <tr>
+                    <th>ST_ID</th>
+                    <th>NAME</th>
+                    <th>DEPT</th>
+                </tr>
+                `;
       rows.forEach((item) => {
         template += `
-              <tr>
-              <td>${item.ST_ID}</td>
-              <td>${item.NAME}</td>
-              <td>${item.DEPT}</td>
-              </tr>
-              `;
+                    <tr>
+                        <th>${item.ST_ID}</th>
+                        <th>${item.NAME}</th>
+                        <th>${item.DEPT}</th>
+                    </tr>
+                    `;
       });
-      template += "</table>";
+      template += `</table>`;
       res.end(template);
+      console.log("The solution is : ", rows);
     } else {
-      console.log("Error while performing Query~!!\n\n");
+      console.log("Error while performing Query~!!");
     }
   });
 });
 
 app.listen(8080, function () {
-  console.log("8080 Port : Server Started~!!\n\n");
+  console.log("8080 Port : Server Started~!!");
 });
